b0dc16309227fd65ec05e3db66a2282671eb8e85
lucaslinyker <lucaslinyker@outlook.com>
2024-07-05 18:44:29 -0300

    :sparkles: feat: Criar exportação das imagens
    Criar arquivo em cada pasta de imagens (blog, products...) contendo código para exportar todas as imagens da pasta, facilitando na importação

---
+++ src/assets/images/blog/export.js
@@ -0,0 +1,4 @@
+export { default as dumbbells } from './dumbbells.webp'
+export { default as clipboard } from './clipboard.webp'
+export { default as training } from './training.webp'
+export { default as weight } from './weight.webp'
---
+++ src/assets/images/products/export.js
@@ -0,0 +1,8 @@
+export { default as tennisBalls } from './tennisBalls.webp'
+export { default as dumbbells } from './dumbbells.webp'
+export { default as tennisRacket } from './tennisRacket.webp'
+export { default as sneakerGreenNike } from './sneakerGreenNike.webp'
+export { default as tennis } from './tennis.webp'
+export { default as soccerBall } from './soccerBall.webp'
+export { default as airForceNike } from './airForceNike.webp'
+export { default as sneakerRedNike } from './sneakerRedNike.webp'

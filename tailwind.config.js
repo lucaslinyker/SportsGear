2d3bc2a18e30920708c7d1d2999f205c99970dca
lucaslinyker <lucaslinyker@outlook.com>
2024-12-04 16:11:48 -0300

    ✨ feat: criar componente MinimumHeader
    - componente de cabeçalho que será usado tanto para a página de login como de registro
    - formatar as variáveis de cores (remover as ')

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,12 +7,20 @@ export default {
         'roboto': ['Roboto', 'sans-serif'],
       },
       colors: {
-        'color0': '#000d16',
-        'color1': '#02416d',
-        'color2': '#0a5483',
-        'color3': '#066699',
-        'color4': '#aedd2b',
-        'color5': '#f8f8ec'
+        color0: '#000d16',
+        color1: '#02416d',
+        color2: '#0a5483',
+        color3: '#066699',
+        color4: '#aedd2b',
+        color5: '#f8f8ec',
+      },
+      animation: {
+        'show-header': 'show-header .5s ease-out',
+      },
+      keyframes: {
+        'show-header': {
+          '0%': { transform: 'translateY(-100%)' },
+        },
       },
     },
   },

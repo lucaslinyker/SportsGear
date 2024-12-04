efcbd963d740e0f633e9ef701d654170a8348d77
lucaslinyker <lucaslinyker@outlook.com>
2024-12-04 17:39:16 -0300

    ✨ feat: criar template SignInUp
    - criar o html
    - criar o css (index.css e tailwind.config.js)
    - futuramente adicionar auth com firebase

---
+++ src/templates/SignInUp.jsx
@@ -0,0 +1,113 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { MinimumHeader } from '../components/MinimumHeader'
+
+export function SignInUp({ title, action }) {
+  useEffect(() => {
+    document.body.classList.add('bg-gradient-to-br', 'from-color0', 'to-color3')
+
+    return () => {
+      document.body.classList.remove(
+        'bg-gradient-to-br',
+        'from-color0',
+        'to-color3'
+      )
+    }
+  }, [])
+
+  return (
+    <>
+      <MinimumHeader />
+
+      {/*  NOTE:  the value 2.375rem refers to the Height / 2 of the header, enough to keep the main centered in the remaining part */}
+      <main className='absolute top-[calc(50%_+_30px)] sm:top-[calc(50%_+_2.375rem)] left-1/2 -translate-x-1/2 -translate-y-1/2 animate-show-main text-color5 bg-color1 p-5 sm:p-10 rounded-lg w-11/12 sm:w-[500px] shadow-1 hover:shadow-2 transition-shadow'>
+        <h1 className='mb-5 font-bold text-2xl sm:text-3xl'>{title}</h1>
+
+        {/*  TODO:  implement the auth with firebase/google */}
+        <aside
+          className='group border rounded-md hover:cursor-not-allowed hover:border-color4'
+          title='not implemented yet'
+        >
+          <button
+            className='w-full flex gap-2 justify-center items-center py-1 group-hover:cursor-not-allowed group-hover:text-color4'
+            type='button'
+            disabled
+          >
+            <i className='fa-brands fa-google fa-xl'></i>
+            <h2>{`${action} com o Google`}</h2>
+          </button>
+        </aside>
+
+        <div className='grid grid-cols-[1fr_auto_1fr] gap-x-4 items-center my-2'>
+          <hr />
+          <p className='font-light tracking-wider'>ou</p>
+          <hr />
+        </div>
+
+        <form id='form' autoComplete='on'>
+          <label htmlFor='email'>E-mail:</label>
+          <input
+            className='w-full h-10 p-2 mt-1 mb-2 rounded text-color0 outline-none transition-[border] duration-75 ease-linear focus:border-[3px] focus:border-color4'
+            type='email'
+            id='email'
+            placeholder='seu.email@gmail.com'
+            autoComplete='email'
+            autoFocus
+            required
+          />
+
+          <label htmlFor='password'>Senha:</label>
+          <input
+            className='w-full h-10 p-2 mt-1 mb-2 rounded text-color0 outline-none transition-[border] duration-75 ease-linear focus:border-[3px] focus:border-color4'
+            type='password'
+            id='password'
+            placeholder='91nu7#ww'
+            autoComplete={
+              action === 'Entrar' ? 'current-password' : 'new-password'
+            }
+            minLength='8'
+            required
+          />
+
+          {action === 'Cadastrar' && (
+            <>
+              <label htmlFor='confirm-password'>Confirmar senha:</label>
+              <input
+                className='w-full h-10 p-2 mt-1 mb-2 rounded text-color0 outline-none transition-[border] duration-75 ease-linear focus:border-[3px] focus:border-color4'
+                type='password'
+                id='confirm-password'
+                placeholder='91nu7#ww'
+                autoComplete='new-password'
+                required
+              />
+            </>
+          )}
+
+          <button
+            className='block mt-2 mb-2 py-2 w-full rounded bg-color4 text-color0 font-bold text-xl hover:bg-color3 hover:text-color5 active:scale-[.99] custom-transition'
+            type='submit'
+          >
+            {action}
+          </button>
+
+          {action === 'Entrar' ? (
+            <div className='flex flex-col mt-4 xs:mt-0 xs:flex-row xs:justify-between'>
+              <Link className='w-fit font-light' to='#reset'>
+                Esqueci a senha
+              </Link>
+              <Link className='w-fit font-light' to='/cadastrar'>
+                Não tem uma conta?
+              </Link>
+            </div>
+          ) : (
+            <div className='text-right'>
+              <Link className='font-light' to='/entrar'>
+                Já tem uma conta?
+              </Link>
+            </div>
+          )}
+        </form>
+      </main>
+    </>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,13 +14,24 @@ export default {
         color4: '#aedd2b',
         color5: '#f8f8ec',
       },
+      screens: {
+        xs: '380px',
+      },
       animation: {
         'show-header': 'show-header .5s ease-out',
+        'show-main': 'show-main .5s ease-out, show-content 1s linear',
       },
       keyframes: {
         'show-header': {
           '0%': { transform: 'translateY(-100%)' },
         },
+        'show-main': {
+          '0%': { left: '-100%' },
+        },
+      },
+      boxShadow: {
+        1: '0 0 10px 0 rgba(0, 0, 0, 0.3)',
+        2: '0 0 13px 1px rgba(0, 0, 0, 0.3)',
       },
     },
   },

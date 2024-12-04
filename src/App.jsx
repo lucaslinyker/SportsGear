4b4dd1bf77be2263f80ed9e6ad5e10ee0ea5667a
lucaslinyker <lucaslinyker@outlook.com>
2024-12-04 18:10:39 -0300

    ✨ feat: criar rotas de login e registro

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,19 @@ import {
 } from 'react-router-dom'
 
 import { NotFound } from './pages/NotFound'
+import { Error } from './pages/Error'
+import { Login } from './routes/Login'
+import { Register } from './routes/Register'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <>
+      <Route path='/entrar' element={<Login />} errorElement={<Error />} />
+      <Route
+        path='/cadastrar'
+        element={<Register />}
+        errorElement={<Error />}
+      />
       <Route path='*' element={<NotFound />} />
     </>
   )
---
+++ src/routes/Register.jsx
@@ -0,0 +1,12 @@
+import { SignInUp } from '../templates/SignInUp'
+
+export function Register() {
+  return (
+    <>
+      <SignInUp
+        title='Cadastre uma conta:'
+        action='Cadastrar'
+      />
+    </>
+  )
+}

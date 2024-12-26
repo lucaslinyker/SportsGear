d51525e5885941c9fec777d383b9bc04a4093e0d
lucaslinyker <lucaslinyker@outlook.com>
2024-12-26 17:05:01 -0300

    ✨ feat: criar componente Features
    componente que irá mostrar os benefícios de comprar na SportsGear

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,10 @@
+import { Features } from './Features.jsx'
+
 export function Main() {
   return (
-    <main className='p-5 md:py-10 md:px-20'></main>
+    <main className='p-5 md:py-10 md:px-20'>
+      <Features />
+      <hr className='border-black/50' />
+    </main>
   )
 }

7be6636d9a559da254454182e9bb95db847f913a
lucaslinyker <lucaslinyker@outlook.com>
2024-12-26 18:10:05 -0300

    ✨ feat: criar componente Brands

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,13 @@
 import { Features } from './Features.jsx'
+import { Brands } from './Brands.jsx'
 
 export function Main() {
   return (
     <main className='p-5 md:py-10 md:px-20'>
       <Features />
       <hr className='border-black/50' />
+
+      <Brands />
     </main>
   )
 }

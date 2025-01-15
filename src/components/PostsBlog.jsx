6485a9651344b699f59a8c5028786fa1843ccf74
lucaslinyker <lucaslinyker@outlook.com>
2025-01-15 20:04:13 -0300

    ✨ feat: criar arquivo de api do PostsBlog

--- src/components/PostsBlog.jsx
+++ src/components/PostsBlog.jsx
@@ -1,33 +1,6 @@
-import * as postsImages from '../assets/images/blog/export.js'
+import { posts } from '../api/postsBlog.js'
 
 import { Link } from 'react-router-dom'
-
-const posts = [
-  {
-    title: 'Aumentando a massa muscular com halteres',
-    alt: 'Anilhas e halteres',
-    date: '28/12/2024',
-    image: postsImages.dumbbells,
-  },
-  {
-    title: 'Como ter uma alimentação saudável',
-    alt: 'Prancheta',
-    date: '18/12/2024',
-    image: postsImages.clipboard,
-  },
-  {
-    title: 'Três maneiras de ter braços maiores',
-    alt: 'Treino de braço com halter',
-    date: '8/12/2024',
-    image: postsImages.training,
-  },
-  {
-    title: 'Anilha: o que é e para que serve',
-    alt: 'Anilha',
-    date: '1/12/2024',
-    image: postsImages.weight,
-  },
-]
 
 export function PostsBlog() {
   return (
